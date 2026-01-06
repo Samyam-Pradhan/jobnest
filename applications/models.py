4ee125aef8b70cc17619298e98308dbd5eff91cf
Samyam-Pradhan <pradhansamyam71@gmail.com>
2026-01-06 10:55:28 +0545

    Add applications model

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Job Seeker Profile
+class JobSeekerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, blank=True)
+    position = models.CharField(max_length=100, blank=True)
+    cv = models.FileField(upload_to='cvs/', blank=True)
+    profile_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+# Employer Profile
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100, blank=True)
+    profile_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
