01d2f0a97ae4cb7691b2eb1b5f400612b817ad64
Samyam-Pradhan <pradhansamyam71@gmail.com>
2026-01-06 20:02:28 +0545

    feat : Add login and logout

--- applications/urls.py
+++ applications/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),   # new
+    path('logout/', views.user_logout, name='logout'), # new
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/', views.profile, name='profile'),
 ]
