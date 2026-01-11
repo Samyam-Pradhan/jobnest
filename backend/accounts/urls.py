1f7b4fb7d91352b2a1387870751d49b4c24ad037
Samyam-Pradhan <pradhansamyam71@gmail.com>
2026-01-11 22:15:04 +0545

    Fix URLs in config and accounts app

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterView, LoginView, JobSeekerProfileView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('api/profile/', JobSeekerProfileView.as_view(), name='jobseeker-profile'),
+    path('profile/', JobSeekerProfileView.as_view(), name='jobseeker-profile'),
 
 ]
